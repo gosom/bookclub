e4ce2357131ed7478fbef53cfed399efadc59c11
Giorgos Komninos <admin@gkomninos.com>
2023-09-23 08:54:55 +0300

    adds postgresql service and migration support

---
+++ scripts/migrations/000002_users_updated_at_trigger.up.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
